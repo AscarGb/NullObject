126a24a133d9f7eed78f2bc193138c85cc982e41
AscarGb <fichtengolz@yandex.ru>
2021-02-06 20:49:00 +0300

    library files

---
+++ NullObject/Empty.cs
@@ -0,0 +1,9 @@
+﻿using Moq;
+
+namespace NullObject
+{
+    public static class Empty<T> where T : class
+    {
+        public static T Value { get; } = new Mock<T>().Object;
+    }
+}
---
+++ NullObject/EmptyCheckExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace NullObject
+{
+    public static class EmptyCheckExtension
+    {
+        public static bool IsEmpty<T>(this T nullified) where T : class =>
+            ReferenceEquals(nullified, Empty<T>.Value);
+    }
+}
